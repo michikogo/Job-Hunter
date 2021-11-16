c857195be46ca68316c3d413e199c311161a430e
michikogo <37466954+michikogo@users.noreply.github.com>
2021-11-16 02:23:39 -0500

    add modal

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -50,35 +50,48 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="dashboard-container">
-      <p>Hello {username}</p>
-      <div className="register-button-div">
+    <div
+      className={
+        showModal
+          ? "dashboard-container modal-background"
+          : "dashboard-container"
+      }
+    >
+      <p className={showModal && "modal-hide"}>Hello {username}</p>
+      <div
+        // className="register-button-div"
+        className={
+          showModal ? "register-button-div modal-hidden" : "register-button-div"
+        }
+      >
         <input
           type="submit"
           value="Add Application"
-          className="register-button"
+          className={
+            showModal ? "modal-hide register-button" : "register-button"
+          }
           onClick={handleShow}
         />
       </div>
-      <div>
-        <div style={{ position: "block" }}>
+      <div style={{ position: "relative" }}>
+        <div className="modal-position">
           {showModal && <Modal username={username} handleClose={handleClose} />}
         </div>
-        <div style={{ position: "block" }}>
+        <div style={{ position: "absolute", zIndex: "-1" }}>
           <p>
             Michiko's Org - 2021-02-17 Settings Access Manager Billing All
             Clusters Get Help Michiko Directory Atlas Realm Charts DEPLOYMENT
             Databases Triggers Data Lake SECURITY Database Access Network Access
-            Advanced MICHIKO'S ORG - 2021-02-17 > DIRECTORY > DATABASES
-            UserSchema VERSION 4.4.10 REGION AWS N. Virginia (us-east-1)
-            Overview Real Time Metrics Collections Search Profiler Performance
-            Advisor Online Archive Command Line Tools DATABASES: 1 COLLECTIONS:
-            2 VISUALIZE YOUR DATA REFRESH Create Database NAMESPACES
-            directory-schema directory-data user-data directory-schema.user-data
-            COLLECTION SIZE: 625B TOTAL DOCUMENTS: 4 INDEXES TOTAL SIZE: 72KB
-            Find Indexes Schema Anti-Patterns 0 Aggregation Search Indexes
-            INSERT DOCUMENT FILTER    QUERY RESULTS 0 System StatusAll Good
-            ©2021 MongoDB, Inc.StatusTermsPrivacyAtlas BlogContact Sales
+            Advanced MICHIKO'S ORG - 2021-02-17 DIRECTORY DATABASES UserSchema
+            VERSION 4.4.10 REGION AWS N. Virginia (us-east-1) Overview Real Time
+            Metrics Collections Search Profiler Performance Advisor Online
+            Archive Command Line Tools DATABASES: 1 COLLECTIONS: 2 VISUALIZE
+            YOUR DATA REFRESH Create Database NAMESPACES directory-schema
+            directory-data user-data directory-schema.user-data COLLECTION SIZE:
+            625B TOTAL DOCUMENTS: 4 INDEXES TOTAL SIZE: 72KB Find Indexes Schema
+            Anti-Patterns 0 Aggregation Search Indexes INSERT DOCUMENT FILT
+            QUERY RESULTS 0 System StatusAll Good ©2021 MongoDB,
+            Inc.StatusTermsPrivacyAtlas BlogContact Sales
           </p>
         </div>
       </div>
