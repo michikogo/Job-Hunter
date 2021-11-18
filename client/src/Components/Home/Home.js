e1ec1b796b017165298815965db8d7e6e7452d55
Michiko Go <michikogo98@gmail.com>
2021-11-17 23:20:13 -0500

    responsive

---
+++ client/src/Components/Home/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { abstract, homeImage } from '../../Assets'
+import RegisterForm from '../Forms/RegisterForm'
+import HomeForm from './HomeForm'
+const Home = () => {
+  return (
+    <div className='home-container'>
+      <div>
+        <img src={abstract} className='home-abstract' />
+        <h1 className='home-h1'>
+          Organize and schedule your job applications with us!
+        </h1>
+        <img src={homeImage} className='home-image' />
+      </div>
+      <div></div>
+      <div className='home-register-section'>
+        <h3 className='home-form'>
+          Sign up now! We love to help you organize your job applications.
+        </h3>
+        <HomeForm />
+      </div>
+    </div>
+  )
+}
+
+export default Home
