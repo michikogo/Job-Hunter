4ff8928d7a26b5af733ab39a9cf9aad140e1ece8
michikogo <37466954+michikogo@users.noreply.github.com>
2021-11-19 12:24:10 -0500

    added logo

--- server/index.js
+++ server/index.js
@@ -1,30 +1,31 @@
 // Import npm packages
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 // Import Routers
-const UserController = require('./Controller/UserController')
-const DirectoryController = require('./Controller/DirectoryController')
+const UserController = require("./Controller/UserController");
+const DirectoryController = require("./Controller/DirectoryController");
 
 // Uses express
-const app = express()
+const app = express();
 
 // Middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-mongoose.connect(
-  'mongodb+srv://admin:1234@userschema.dqeqq.mongodb.net/directory-schema?retryWrites=true&w=majority'
-)
+const MONGO_URI = process.env.MONGO_URI;
+mongoose.connect(MONGO_URI);
 
-mongoose.connection.once('open', () => {
-  console.log('MongoDB connection established successfully')
-})
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection established successfully");
+});
 
-app.use('/user', UserController)
-app.use('/directory', DirectoryController)
+app.use("/user", UserController);
+app.use("/directory", DirectoryController);
 
-app.listen(8000, () => {
-  console.log('Listen to 8000')
-})
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log("Listen to " + PORT);
+});
