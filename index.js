995f8673c80cf2f4aca4bfedd9049e40f0d279d6
michikogo <37466954+michikogo@users.noreply.github.com>
2021-11-19 14:26:23 -0500

    DB slow

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use("/directory", DirectoryController);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
