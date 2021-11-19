8b6671541a92056b11633d4a87fea7c09213b1d4
michikogo <37466954+michikogo@users.noreply.github.com>
2021-11-19 12:59:12 -0500

    production

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ app.use("/user", UserController);
 app.use("/directory", DirectoryController);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
